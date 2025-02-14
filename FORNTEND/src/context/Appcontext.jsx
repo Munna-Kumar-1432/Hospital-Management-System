ebd37b4bc818de398967c69982573d3bde616f1d
Munna Kumar <munnakumar6204835946@gmail.com>
2025-02-14 10:36:00 +0530

    update

---
+++ FORNTEND/src/context/Appcontext.jsx
@@ -0,0 +1,73 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+    const currencySymbol = '₹'
+    const backendURL = import.meta.env.VITE_BACKEND_URL;
+    const [doctors,setDoctors] = useState([])
+    const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+    const [userData,setUserData] = useState(false)
+
+    const getDoctorsData = async () =>{
+        try {
+            const {data} = await axios.get(backendURL+'/api/doctor/list')
+            if(data.success){
+                setDoctors(data.doctors)
+            }else{
+                toast.error(error.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+           console.log(error.message) 
+        }
+    }
+
+    const loadUserProfileData = async () =>{
+        try {
+            const {data} = await axios.get(backendURL+'/api/user/get-profile',{headers:{token}});
+            console.log(data)
+            
+            if(data.success){
+              setUserData(data.userData)
+            }else{
+                toast.error(error.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error.message) 
+        }
+    }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,
+        setToken,
+        backendURL,
+        userData,
+        setUserData,
+        loadUserProfileData,
+        getDoctorsData
+    }
+
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
+
+    useEffect(()=>{
+        if(token){
+            loadUserProfileData()
+        }else{
+            setUserData(false)
+        }
+    },[token])
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
